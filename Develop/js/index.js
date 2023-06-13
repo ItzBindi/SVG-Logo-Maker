7d9e8734c0da784de5ae26b339823de6dbcbe72c
Suraj <snair306@gmail.com>
2023-06-13 16:30:32 -0500

    got it working just not correctly

--- Develop/js/index.js
+++ Develop/js/index.js
@@ -1,15 +1,60 @@
 const inquirer = require("inquirer");
 
 const fs = require("fs");   
-const questions = require("./questions");
-const genShapes = require("./shapes");
+const shapes = require("./shapes.js");
 
-inquirer
-    .prompt(questions)
 
+
+const questions = [
+    {
+        type: 'string',
+        message: 'What is your logo text?(up to 3 characters)',
+        name: 'text',
+    },
+    {
+        type: 'string',
+        message: 'What is the background color of your logo?',
+        name: 'color',
+    },
+    {
+        type: 'string',
+        message: 'What is the color of your logo text?',
+        name: 'textColor',
+    },
+    {
+        type: 'checkbox',
+        message: "What is the shape of your logo?",
+        choices: ['square', 'circle', 'triangle'],
+        name: 'shape',
+    }
+
+]
+
+
+
+inquirer.prompt(questions)
     .then((response) => {
         console.log(response);
-        fs.writeFile('logo.svg', genShapes(response), function (err) {
-            err ? console.log(err) : console.log("success");
+        const selectedShape = response.shape;
+        let svgShape = ''
+        selectedShape.forEach((shape) => {
+        if (shape === 'square') {
+            svgShape = shapes.createSquare(response);
+          } else if (shape === 'circle') {
+            svgShape = shapes.createCircle(response);
+          } else if (shape === 'triangle') {
+            svgShape = shapes.createTriangle(response);
+          } else {
+            console.log('Invalid shape input.');
+            return;
+          }
+          fs.writeFile('logo.svg', svgShape, function (err) {});
+        });
+        
         })
-    })
+
+    .catch((err) => {
+        console.log(err);
+    });
+
+    
--- Develop/js/shapes.js
+++ Develop/js/shapes.js
@@ -1,11 +1,31 @@
-function genShapes(res){
+function createCircle(response) {
+    return`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <circle cx="150" cy="100" r="80" fill="${response.color}" />
+  
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+  
+  </svg>`
+}
+function createSquare(response) {
     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-    <polygon points="150, 18 244, 182 56, 182" fill="bisque" />
+    <rect x="90" y="40" width="120" height="120" fill="${response.color}" />
   
-    <text x="150" y="150" font-size="60" text-anchor="middle" fill="#3a0467">${res.text}</text>
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
   
   </svg>`
 }
 
-module.exports = genShapes;
+function createTriangle(response) {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <polygon points="150, 18 244, 182 56, 182" fill="${response.color}" />
+  
+    <text x="150" y="150" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+  
+  </svg>`
+}
+
+module.exports = {createCircle, createSquare, createTriangle};
+
